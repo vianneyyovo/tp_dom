b8704baee4aa5b2fa5a7d4ba89fd391feed4f4a5
viaanneyyovo <vianney.yovo@epitech.eu>
2024-09-21 02:28:24 +0000

    [UPDATE]: first demo for dom tp project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 // First demo
 document.getElementById("text").innerHTML = "My first set propriety";
 
-// Second demo
-
-
 //Copyright
 const copyright = document.querySelector("footer");
 copyright.innerHTML = "&copy; "
